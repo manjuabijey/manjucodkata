767c14bad3bc27c0e5a6e94b453bbbaff156e0bd
manjuabijey <50616438+manjuabijey@users.noreply.github.com>
2019-05-14 15:20:58 +0530

    Update oddoreven.c

--- oddoreven.c
+++ oddoreven.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
- int main()
+#include<stdio.h>
+void main()
 {
-   int n;
-   
-   printf("Enter an integer\n");
-   scanf("%d", &n);
-   
-   if (n%2 == 0)
-      printf("Even\n");
-   else
-      printf("Odd\n");
+int n;   
+printf("Enter an integer\n");
+scanf("%d", &n);
+if (n%2 == 0)
+printf("Even\n");
+else
+printf("Odd\n");
 }
